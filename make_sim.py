7b82e53a1d1b441a09612702400cd0837c7ac769
Kangjunseo <mind2749@naver.com>
2020-09-14 14:59:15 +0900

    make def for similarity

--- make_sim.py
+++ make_sim.py
@@ -1,15 +1,18 @@
 import json
 import pandas as pd
+import os
 
-with open('datasets/caption/train_cap.json') as train_cap:
+with open('datasets/caption/train_cap_sim.json') as train_cap:
     train_cap = json.load(train_cap)
 
-with open('datasets/caption/val_cap.json') as val_cap:
+with open('datasets/caption/val_cap_sim.json') as val_cap:
     val_cap = json.load(val_cap)
 
-with open('datasets/caption/test_cap.json') as test_cap:
+with open('datasets/caption/test_cap_sim.json') as test_cap:
     test_cap = json.load(test_cap)
 
+with open('datasets/caption/test_dev_cap_sim.json') as test_dev_cap:
+    test_dev_cap = json.load(test_dev_cap)
 
 ####
 """
@@ -22,14 +25,151 @@ with open('datasets/caption/train_qacap_sim.json') as train_qacap_sim:
 with open('datasets/caption/val_qacap_sim.json') as val_qacap_sim:
     val_qacap_sim = json.load(val_qacap_sim)
 
+
+def make_similarity(limit, sim): #sim: q_similarity or total_similarity
+
+    with open('datasets/caption/train_qacap_sim.json') as train_qacap_sim:
+        train_qacap_sim = json.load(train_qacap_sim)
+
+    with open('datasets/caption/val_qacap_sim.json') as val_qacap_sim:
+        val_qacap_sim = json.load(val_qacap_sim)
+
+    for i in train_qacap_sim['data']:
+        # print(i)
+        if i[sim] < limit:  # 숫자 변경
+            i['caption'] = ''
+
+    for i in val_qacap_sim['data']:
+        # print(i)
+        if i[sim] < limit:  # 숫자 변경
+            i['caption'] = ''
+
+    for i in train_qacap_sim['data']:
+        i['question'] = i['question'] + ' ' + i['caption']
+
+    for i in val_qacap_sim['data']:
+        i['question'] = i['question'] + ' ' + i['caption']
+
+    df_train_t = pd.DataFrame(train_qacap_sim['data'])
+    df_val_t = pd.DataFrame(val_qacap_sim['data'])
+
+    del df_train_t['caption']
+    del df_train_t['q_similarity']
+    del df_train_t['a_similarity']
+    del df_train_t['total_similarity']
+    del df_train_t['multiple_choice_answer']
+
+    del df_val_t['caption']
+    del df_val_t['q_similarity']
+    del df_val_t['a_similarity']
+    del df_val_t['total_similarity']
+    del df_val_t['multiple_choice_answer']
+
+    if not (os.path.isdir(os.path.join('datasets/caption/under',str(limit)))):
+        os.makedirs(os.path.join('datasets/caption/under',str(limit)))
+    df_train_t.to_json(os.path.join('datasets/caption/under', str(limit), 'train_'+ sim + '.json'), orient='table')
+    df_train_t.to_json(os.path.join('datasets/caption/under', str(limit), 'val_' + sim + '.json'), orient='table')
+
+    with open(os.path.join('datasets/caption/under', str(limit),'train_'+ sim + '.json')) as train_cap:
+        train_cap = json.load(train_cap)
+
+    for i in train_cap['data']:
+        del i['level_0']
+        del i['index']
+
+    with open(os.path.join('datasets/caption/under', str(limit), 'val_'+ sim + '.json')) as val_cap:
+        val_cap = json.load(val_cap)
+
+    for i in val_cap['data']:
+        del i['level_0']
+        del i['index']
+
+    with open(os.path.join('datasets/caption/under', str(limit), 'train_'+ sim + '.json'), 'w') as f:
+        json.dump(train_cap, f)
+
+    with open(os.path.join('datasets/caption/under', str(limit), 'val_'+ sim + '.json'), 'w') as f2:
+        json.dump(val_cap, f2)
+
+
+make_similarity(0.78, 'q_similarity')
+
+for i in range(76,100):
+    make_similarity(i/100, 'q_similarity')
+    make_similarity(i/100, 'total_similarity')
+
+def make_similarity_test(limit, sim): #sim: q_similarity or total_similarity
+
+    with open('datasets/caption/test_cap_sim.json') as test_cap:
+        test_cap = json.load(test_cap)
+
+    with open('datasets/caption/test_dev_cap_sim.json') as test_dev_cap:
+        test_dev_cap = json.load(test_dev_cap)
+
+    for i in test_cap['data']:
+        # print(i)
+        if i[sim] < limit:  # 숫자 변경
+            i['caption'] = ''
+
+    for i in test_dev_cap['data']:
+        # print(i)
+        if i[sim] < limit:  # 숫자 변경
+            i['caption'] = ''
+
+    for i in test_cap['data']:
+        i['question'] = i['question'] + ' ' + i['caption']
+
+    for i in test_dev_cap['data']:
+        i['question'] = i['question'] + ' ' + i['caption']
+
+    df_test = pd.DataFrame(test_cap['data'])
+    df_test_dev = pd.DataFrame(test_dev_cap['data'])
+
+    del df_test['caption']
+    del df_test['similarity']
+
+    del df_test_dev['caption']
+    del df_test_dev['similarity']
+
+    if not (os.path.isdir(os.path.join('datasets/caption/under',str(limit)))):
+        os.makedirs(os.path.join('datasets/caption/under',str(limit)))
+
+    df_test.to_json(os.path.join('datasets/caption/under', str(limit), 'test_q_'+ sim + '.json'), orient='table')
+
+    with open(os.path.join('datasets/caption/under', str(limit), 'test_q_'+ sim + '.json')) as test_cap:
+        test_cap = json.load(test_cap)
+
+    df_test_dev.to_json(os.path.join('datasets/caption/under', str(limit), 'test_dev_q_'+ sim + '.json'), orient='table')
+
+    with open(os.path.join('datasets/caption/under', str(limit), 'test_dev_q_'+ sim + '.json')) as test_dev_cap:
+        test_dev_cap = json.load(test_dev_cap)
+
+    for i in test_cap['data']:
+        del i['index']
+
+    for i in test_dev_cap['data']:
+        del i['index']
+
+    with open(os.path.join('datasets/caption/under', str(limit), 'test_q_'+ sim + '.json'), 'w') as f3:
+        json.dump(test_cap, f3)
+
+    with open(os.path.join('datasets/caption/under', str(limit), 'test_dev_q_'+ sim + '.json'), 'w') as f4:
+        json.dump(test_dev_cap, f4)
+
+make_similarity_test(0.76, 'similarity')
+
+for i in range(77,100):
+    make_similarity_test(i/100, 'similarity')
+
+
+
 for i in train_qacap_sim['data']:
     # print(i)
-    if i['q_similarity'] < 0.77:   #숫자 변경
+    if i['q_similarity'] < 0.75:   #숫자 변경
         i['caption'] = ''
 
 for i in val_qacap_sim['data']:
     # print(i)
-    if i['q_similarity'] < 0.77:   #숫자 변경
+    if i['q_similarity'] < 0.75:   #숫자 변경
         i['caption'] = ''
 
 for i in train_qacap_sim['data']:
@@ -37,42 +177,68 @@ for i in train_qacap_sim['data']:
 
 for i in val_qacap_sim['data']:
     i['question'] = i['question']+ ' ' + i['caption']
+
+# for i in train_qacap_sim['data']:
+#     del i['index']
+#
+# for i in val_qacap_sim['data']:
+#     del i['index']
 
 df_train_t = pd.DataFrame(train_qacap_sim['data'])
 df_val_t = pd.DataFrame(val_qacap_sim['data'])
 
 ##############3
 
-df_train_t = df_train_t.drop(['index', 'image_id', 'question_id'], axis='columns')
+# df_train_t = df_train_t.drop(['index', 'image_id', 'question_id'], axis='columns')
 df_train_t = df_train_t.sort_values(by='total_similarity',ascending=False)
 df_train_t[df_train_t.total_similarity >= 0.77]  #184145 개 41%
-df_train_t[df_train_t.q_similarity >= 0.77] # 329721개 74%
+df_train_t[df_train_t.q_similarity >= 0.81] # 329721개 74% 가나다라마바사 아자차카타파하
 
-df_val_t = df_val_t.drop(['index', 'image_id', 'question_id'], axis='columns')
+# df_val_t = df_val_t.drop([ 'image_id', 'question_id'], axis='columns')
 df_val_t[df_val_t.total_similarity >= 0.77]  #89274 개 42%
-df_val_t[df_val_t.q_similarity >= 0.77]  #160325개 75%
+df_val_t[df_val_t.q_similarity >= 0.81]  #160325개 75%
 
 df_train_t[round(df_train_t.total_similarity,2) == 0.95][['question','caption']]
 #############3
 
 
 df_train_t.iloc[0]
-del df_train_t['index']
+# del df_train_t['index']
 del df_train_t['caption']
 del df_train_t['q_similarity']
 del df_train_t['a_similarity']
 del df_train_t['total_similarity']
 del df_train_t['multiple_choice_answer']
 
-del df_val_t['index']
+# del df_val_t['index']
 del df_val_t['caption']
 del df_val_t['q_similarity']
 del df_val_t['a_similarity']
 del df_val_t['total_similarity']
 del df_val_t['multiple_choice_answer']
 
-df_train_t.to_json('datasets/caption/under/train_q_0.77.json', orient='table')
-df_val_t.to_json('datasets/caption/under/val_q_0.77.json', orient='table')
+df_train_t.to_json('datasets/caption/under/0.75/train_q.json', orient='table')
+df_val_t.to_json('datasets/caption/under/0.75/val_q.json', orient='table')
+
+with open('datasets/caption/under/0.75/train_q.json') as train_cap:
+    train_cap = json.load(train_cap)
+
+for i in train_cap['data']:
+    del i['level_0']
+    # del i['index']
+
+with open('datasets/caption/under/0.75/val_q.json') as val_cap:
+    val_cap = json.load(val_cap)
+
+for i in val_cap['data']:
+    del i['level_0']
+    del i['index']
+
+with open('datasets/caption/under/0.75/train_q.json', 'w') as f:
+    json.dump(train_cap, f)
+
+with open('datasets/caption/under/0.75/val_q.json', 'w') as f2:
+    json.dump(val_cap, f2)
 ####
 
 # df_tv = pd.concat([df_train, df_val], ignore_index=True)
@@ -90,20 +256,27 @@ df_val_t.to_json('datasets/caption/under/val_q_0.77.json', orient='table')
 ######
 for i in train_cap['data']:
     # print(i)
-    if i['similarity'] < 0.9:   #숫자 변경
+
+    if i['similarity'] < 0.85:   #숫자 변경
         i['caption'] = ''
 
 for i in val_cap['data']:
     # print(i)
-    if i['similarity'] < 0.9:
+
+    if i['similarity'] < 0.85:
         i['caption'] = ''
 
 for i in test_cap['data']:
     # print(i)
-    if i['similarity'] < 0.77:
+
+    if i['similarity'] < 0.85:
         i['caption'] = ''
 
+for i in test_dev_cap['data']:
+    # print(i)
 
+    if i['similarity'] < 0.85:
+        i['caption'] = ''
 
 #############3
 """Question + Caption """
@@ -117,7 +290,8 @@ for i in val_cap['data']:
 for i in test_cap['data']:
     i['question'] = i['question']+ ' ' + i['caption']
 
-
+for i in test_dev_cap['data']:
+    i['question'] = i['question']+ ' ' + i['caption']
 
 # with open('datasets/caption/train_cap_under08.json', 'w') as f:
 #     json.dump(train_cap, f)
@@ -131,8 +305,9 @@ for i in test_cap['data']:
 df_train = pd.DataFrame(train_cap['data'])
 df_val = pd.DataFrame(val_cap['data'])
 df_test = pd.DataFrame(test_cap['data'])
+df_test_dev = pd.DataFrame(test_dev_cap['data'])
 
-df_train.iloc[0]
+
 del df_train['caption']
 del df_train['similarity']
 
@@ -142,40 +317,54 @@ del df_val['similarity']
 del df_test['caption']
 del df_test['similarity']
 
-df_train.to_json('datasets/caption/train_under09.json', orient='table')
+del df_test_dev['caption']
+del df_test_dev['similarity']
 
-with open('datasets/caption/train_under09.json') as train_cap:
+df_train.to_json('datasets/caption/under/train_0.85.json', orient='table')
+
+with open('datasets/caption/under/train_0.85.json') as train_cap:
     train_cap = json.load(train_cap)
 
 for i in train_cap['data']:
     del i['index']
 
-df_val.to_json('datasets/caption/val_under09.json', orient='table')
+df_val.to_json('datasets/caption/under/val_0.85.json', orient='table')
 
-with open('datasets/caption/val_under09.json') as val_cap:
+with open('datasets/caption/under/val_0.85.json') as val_cap:
     val_cap = json.load(val_cap)
 
 for i in val_cap['data']:
     del i['index']
 
-val_cap
+# val_cap
 
-df_test.to_json('datasets/caption/test_under09.json', orient='table')
-with open('datasets/caption/test_under09.json') as test_cap:
+df_test.to_json('datasets/caption/under/test_0.85.json', orient='table')
+
+with open('datasets/caption/under/test_0.85.json') as test_cap:
     test_cap = json.load(test_cap)
+
+df_test_dev.to_json('datasets/caption/under/test_dev_0.85.json', orient='table')
+
+with open('datasets/caption/under/test_dev_0.85.json') as test_dev_cap:
+    test_dev_cap = json.load(test_dev_cap)
 
 for i in test_cap['data']:
     del i['index']
 
-with open('datasets/caption/train_under09.json', 'w') as f:
+for i in test_dev_cap['data']:
+    del i['index']
+
+with open('datasets/caption/under/train_0.85.json', 'w') as f:
     json.dump(train_cap, f)
 
-with open('datasets/caption/val_under09.json', 'w') as f2:
+with open('datasets/caption/under/val_0.85.json', 'w') as f2:
     json.dump(val_cap, f2)
 
-with open('datasets/caption/test_under09.json', 'w') as f3:
+with open('datasets/caption/under/test_0.85.json', 'w') as f3:
     json.dump(test_cap, f3)
 
+with open('datasets/caption/under/test_dev_0.85.json', 'w') as f4:
+    json.dump(test_dev_cap, f4)
 
 """
 path_cfgs.py 경로 바꾸고
